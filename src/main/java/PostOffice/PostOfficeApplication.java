2a8868372b32be454084e4fe340973d2e4b99414
Михаил Кондрашин <Kondrashin.mihail@outlook.com>
2022-07-04 14:02:54 +0400

    Fix

--- src/main/java/PostOffice/PostOfficeApplication.java
+++ src/main/java/PostOffice/PostOfficeApplication.java
@@ -2,18 +2,10 @@ package PostOffice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@Controller
 public class PostOfficeApplication{
     public static void main(String[] args) {
         SpringApplication.run(PostOfficeApplication.class, args);
-    }
-
-    @GetMapping("/")
-    public String onStartUp() {
-        return "redirect:/swagger-ui/index.html";
     }
 }
